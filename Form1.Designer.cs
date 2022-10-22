a8238df59b8b644064fa2021e1f129fd301ab958
Pavel <Pavel@DESKTOP-DOB86IK>
2022-10-23 01:35:51 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace assignment7_PavelGolovan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // openFileButton
+            // 
+            this.openFileButton.Font = new System.Drawing.Font("Garamond", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.openFileButton.Location = new System.Drawing.Point(59, 31);
+            this.openFileButton.Name = "openFileButton";
+            this.openFileButton.Size = new System.Drawing.Size(475, 40);
+            this.openFileButton.TabIndex = 0;
+            this.openFileButton.Text = "Open a text file with the student\'s answers.";
+            this.openFileButton.UseVisualStyleBackColor = true;
+            this.openFileButton.Click += new System.EventHandler(this.openFileButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Font = new System.Drawing.Font("Garamond", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.exitButton.Location = new System.Drawing.Point(504, 90);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 40);
+            this.exitButton.TabIndex = 1;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(591, 142);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.openFileButton);
+            this.Font = new System.Drawing.Font("Garamond", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Name = "Form1";
+            this.Text = "Driver\'s License Exam";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button openFileButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace assignment7_PavelGolovan
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openFileButton_Click(object sender, EventArgs e)
+        {
+            int row, col, correct, incorrect, index;
+
+            correct = 0;
+            incorrect = 0;
+            index = 0;
+
+            const int ROWS = 4;
+            const int COLS = 5;
+
+            string[,] correctAnswers = { {"1. B", "2. D", "3. A", "4. A", "5. C"},
+                                         {"6. A", "7. B", "8. A", "9. C", "10. D"},
+                                         {"11. B", "12. C", "13. D", "14. A", "15. D"},
+                                         {"16. C", "17. C", "18. B", "19. D", "20. A"}
+                                       };
+            string[,] studentAnswers = new string[ROWS, COLS];
+
+            StreamReader inputFile;
+            try
+            {
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    inputFile = File.OpenText(openFileDialog1.FileName);
+                    for (row = 0; row < ROWS; row++)
+                    {
+                        for (col = 0; col < COLS; col++)
+                        {
+                            studentAnswers[row, col] = inputFile.ReadLine();
+                            if (studentAnswers[row, col] == correctAnswers[row, col])
+                            {
+                                correct++;
+                            }
+                            else
+                            {
+                                incorrect++;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            string[] incorrectAnswers = new string[incorrect];
+            while (index < incorrect)
+            {
+                for (row = 0; row < ROWS; row++)
+                {
+                    for (col = 0; col < COLS; col++)
+                    {
+                        if (studentAnswers[row, col] != correctAnswers[row, col])
+                        {
+                            incorrectAnswers[index] = (row * COLS + (col + 1)).ToString();
+                            index++;
+                        }
+                    }
+                }
+            }
+
+            if (correct < 15)
+            {
+                MessageBox.Show("Student failed the exam." +
+                    "\n\nTotal number of correctly answered questions - " + correct +
+                    ";\nTotal number of incorrectly answered questions - " + incorrect +
+                    ";\nFollowing questions were answered incorrectly: " + string.Join(", ", incorrectAnswers) + ".");
+            }
+            else if (incorrect == 0)
+            {
+                MessageBox.Show("Student passed the exam." +
+                    "\n\nTotal number of correctly answered questions - " + correct +
+                    ";\nTotal number of incorrectly answered questions - " + incorrect +
+                    ";\nFollowing questions were answered incorrectly: none.");
+            }
+            else
+            {
+                MessageBox.Show("Student passed the exam." +
+                    "\n\nTotal number of correctly answered questions - " + correct +
+                    ";\nTotal number of incorrectly answered questions - " + incorrect +
+                    ";\nFollowing questions were answered incorrectly: " + string.Join(", ", incorrectAnswers) + ".");
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
